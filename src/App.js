3f68badc23582bdbcd21b3ff70ee2974d590461e
Humble-Preacher <61693488+Humble-Preacher@users.noreply.github.com>
2021-05-13 03:27:40 +0000

    added componenets and aseets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Bluebar from "./components/Bluebar";
+import Getstarted from "./components/GetStarted";
+import NavBar from "./components/Navbar";
+import Hero from "./components/Hero";
+import CoinTable from "./components/CoinTable";
+import Earn from "./components/Earn";
+import Portfolio from "./components/Portfolio";
+import Startearning from "./components/Startearning"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="bg-white h-screen">
+			<NavBar />
+			<Hero />
+			<CoinTable />
+			<Earn />
+			<Portfolio />
+			<Bluebar />
+			<Getstarted />
+			<Startearning />
+		</div>
+	);
 }
 
 export default App;
